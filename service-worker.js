4699240a1f309f319344be55b5dab1ec2ff5cea1
shehaby <89967850+leaderabdelaly@users.noreply.github.com>
2026-01-07 17:13:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("ssp-v3").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./license.js",
-        "./lang.json"
-      ]);
-    })
-  );
-});
+self.addEventListener("fetch", ()=>{});
