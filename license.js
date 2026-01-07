97e9dad04165347f13cf81f147a3303b15b4b7a7
shehaby <89967850+leaderabdelaly@users.noreply.github.com>
2026-01-07 16:00:52 +0200

    Update license.js

--- license.js
+++ license.js
@@ -1,1 +1,22 @@
-﻿
+﻿const LICENSE_KEY = "shehaby_license";
+const MASTER_KEY = "SHEHABY-PRO-2026";
+
+function isProUser() {
+  return localStorage.getItem(LICENSE_KEY) === "PRO";
+}
+
+function activatePro(key) {
+  if (key === MASTER_KEY) {
+    localStorage.setItem(LICENSE_KEY, "PRO");
+    return true;
+  }
+  return false;
+}
+
+function canAddShot(count) {
+  return isProUser() || count < 10;
+}
+
+function canShowTreatment() {
+  return isProUser();
+}
